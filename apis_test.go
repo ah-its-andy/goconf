b464c547f2efd0fa4051b6a212a0d7eaf9acff68
andy <mykge@outlook.com>
2022-05-10 17:12:36 +0800

    update unit test

--- apis_test.go
+++ apis_test.go
@@ -45,3 +45,79 @@ func TestSectionBindMapShouldPassed(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, "value", recv["key"])
 }
+
+type bindTestStrcut struct {
+	Key string `json:"key"`
+}
+
+func TestSectionBindStructShouldPassed(t *testing.T) {
+	initTestConfig(newTestReader())
+	sec := goconf.GetSection("sec")
+	assert.NotNil(t, sec)
+
+	var recv bindTestStrcut
+	err := sec.Bind(&recv)
+	assert.Nil(t, err)
+	assert.Equal(t, "value", recv.Key)
+}
+
+func TestGetStringShouldPassed(t *testing.T) {
+	initTestConfig(newTestReader())
+
+	v, ok := goconf.GetString("name")
+	assert.True(t, ok)
+	assert.Equal(t, "test", v)
+
+	v, ok = goconf.GetString("age")
+	assert.True(t, ok)
+	assert.Equal(t, "18", v)
+}
+
+func TestGetStringNotExistsShouldPassed(t *testing.T) {
+	initTestConfig(newTestReader())
+
+	v, ok := goconf.GetString("notexists")
+	assert.False(t, ok)
+	assert.Equal(t, "", v)
+}
+
+func TestGetDefaultShouldPassed(t *testing.T) {
+	initTestConfig(newTestReader())
+
+	v := goconf.GetStringOrDefault("notexists", "default")
+	assert.Equal(t, "default", v)
+}
+
+func TestCastToIntShouldPassed(t *testing.T) {
+	initTestConfig(newTestReader())
+
+	v, ok := goconf.Cast("age", goconf.IntConversion)
+
+	assert.True(t, ok)
+	assert.Equal(t, 18, v)
+}
+
+func TestCastOrDefaultShouldPassed(t *testing.T) {
+	initTestConfig(newTestReader())
+
+	v := goconf.CastOrDefault("notexists", 18, goconf.IntConversion)
+	assert.Equal(t, 18, v)
+}
+
+func TestCastNotExists(t *testing.T) {
+	initTestConfig(newTestReader())
+
+	v, ok := goconf.Cast("notexists", goconf.IntConversion)
+	assert.False(t, ok)
+	assert.Equal(t, nil, v)
+}
+
+func TestCastShouldPanicBeforeInitialized(t *testing.T) {
+	defer func() {
+		if r := recover(); r != nil {
+			assert.Equal(t, "configuration is not initialized", r)
+		}
+	}()
+
+	goconf.Cast("name", goconf.IntConversion)
+}
